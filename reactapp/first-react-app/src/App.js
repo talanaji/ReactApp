b3b77739addc39c7b823ea739901e445891226b9
talanaji <talanaji@gmail.com>
2021-05-04 01:05:45 +0200

    adding keys to list items

--- reactapp/first-react-app/src/App.js
+++ reactapp/first-react-app/src/App.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
+import './fontawesome/css/all.min.css';
+import './css/templatemo-comparto.css';
 function Header(props) {
   return (
     <div class="tm-site-header tm-mb-1">
@@ -7,32 +10,14 @@ function Header(props) {
       </div>
       <div class="tm-nav-container tm-bg-color-8">
         <nav class="tm-nav" id="tm-nav">
-          <ul>
-            <li class="tm-nav-item current">
-              <a href="#about" class="tm-nav-link">
-                <span class="tm-mb-1">.01</span>
-                <span>About</span>
+          <ul >
+            {console.log(menu)}
+            {props.menu.map((menu_el)=><li key={menu_el.id} class="tm-nav-item current">
+              <a href={`#`+menu_el.id} class="tm-nav-link"> 
+                <span>{menu_el.title}</span>
               </a>
-            </li>
-            <li class="tm-nav-item">
-              <a href="#services" class="tm-nav-link">
-                <span class="tm-mb-1">.02</span>
-                <span>Services</span>
-              </a>
-            </li>
-            <li class="tm-nav-item">
-              <a href="#gallery" class="tm-nav-link">
-                <span class="tm-mb-1">.03</span>
-                <span>Gallery</span>
-              </a>
-            </li>
-            <li class="tm-nav-item">
-              <a href="#contact" class="tm-nav-link">
-                <span class="tm-nav-text tm-mb-1">.04</span>
-                <span class="tm-nav-text">Contact</span>
-              </a>
-            </li>
-          </ul>
+            </li>)}
+           </ul>
         </nav>
       </div>
     </div>
@@ -41,7 +26,21 @@ function Header(props) {
 
 function Main() {
   return (
-
+ <section class="tm-mb-1" id="about">
+            <div class="tm-row tm-about-row">
+            <div class="tm-section-1-l">
+                     <img src="img/comparto-image-01.jpg" alt="About image" class="tm-img-responsive" />
+                </div>
+                <article class="tm-section-1-r tm-bg-color-8">
+                    <h2 class="tm-mb-2 tm-title-color">.01 Comparto CSS Layout</h2>
+                    <p><a rel="nofollow" href="https://templatemo.com/tm-544-comparto" target="_parent">Comparto</a> is a custom light-weight CSS layout for your website. You can easily adapt and use this for your commercial or personal websites. Feel free to use it.</p>
+                    <p>You cannot redistribute this template ZIP file in any template collection website. Please <a rel="nofollow" href="https://templatemo.com/contact" target="_parent">contact TemplateMo</a> if you have any question.</p>
+                    <p>Nunc sed gravida elit. Curabitur rutrum elit id lobortis viverra. Fusce at libero dui.</p>
+                    
+                    <a href="#services" class="tm-btn tm-btn-1 tm-link-to-services">More Detail</a>
+                </article>
+            </div>
+        
     <div class="tm-bg-color-1 tm-mb-1 tm-row tm-social-row">
       <div class="tm-icon">
         <div class="tm-icon-inner">
@@ -69,6 +68,7 @@ function Main() {
       </div>
     </div>
 
+    </section>
   );
 }
 
@@ -79,11 +79,19 @@ function Footer(props) {
     </footer>
   );
 }
-
+const menu=[
+  "About",
+  "Services",
+  "Gallery",
+  "Contact" 
+  
+];
+const menuObjects = menu.map((menu_el,i)=>({id:i, title:menu_el})); 
+console.log(menuObjects);
 function App() {
   return (
     <div className="App">
-      <Header name="Comparto" />
+      <Header name="Comparto" menu={menuObjects} />
       <Main />
       <Footer year ={new Date().getFullYear()} />
     </div>
